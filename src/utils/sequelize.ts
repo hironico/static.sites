1c558d54b008a5d13ec00f12e13d002b700dfef4
hironico <hironico@github.com>
2022-11-22 21:07:36 +0100

    catch a rare exception while connecting

--- src/utils/sequelize.ts
+++ src/utils/sequelize.ts
@@ -14,36 +14,40 @@ export const getSequelize = (): Sequelize => {
     // ensure configuration is loaded whenever we rscall this script
     dotenv.config();
 
-    const hostname = process.env.DB_HOSTNAME; 
+    const hostname = process.env.DB_HOSTNAME;
     const username = process.env.DB_USER;
     const password = process.env.DB_PASSWORD;
     const database = process.env.DB_DATABASE;
     const portnum = process.env.DB_PORT;
     const dbType = process.env.DB_TYPE;
-    
+
     _sequelize = getSequelizeFromParams(hostname, username, password, database, portnum, dbType);
     return _sequelize;
 }
 
-const getSequelizeFromParams = (hostname: string, username: string, password: string, database: string, portnum: string, dbType: string): Sequelize => {    
+const getSequelizeFromParams = (hostname: string, username: string, password: string, database: string, portnum: string, dbType: string): Sequelize => {
     console.info(`Connecting ${username} @ ${hostname} / ${database}`);
 
     const port = Number.parseInt(portnum);
     const dialect = 'MSSQL' === dbType.toUpperCase() ? 'mssql' : 'postgres';
 
-    let sequelize = new Sequelize(database, username, password, {
-        host: hostname,
-        port: port,
-        dialect: dialect,        
-        sync: {
-            force: false
-        },
-        pool: {
-            max: 9,
-            min: 1,
-            idle: 10000,
-        }
-    });
+    try {
+        let sequelize = new Sequelize(database, username, password, {
+            host: hostname,
+            port: port,
+            dialect: dialect,
+            sync: {
+                force: false
+            },
+            pool: {
+                max: 9,
+                min: 1,
+                idle: 10000,
+            }
+        });
 
-    return sequelize;
+        return sequelize;
+    } catch (error) {
+        return null;
+    }
 }
