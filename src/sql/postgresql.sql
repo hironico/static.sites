68fb58633b4bd2074ec3621cba7e467ded51e81b
hironico <hironico@hironico.net>
2023-03-03 12:18:31 +0100

    remeved the ip v4 null constraint to allow ipv6

--- src/sql/postgresql.sql
+++ src/sql/postgresql.sql
@@ -1,17 +1,30 @@
-CREATE TABLE WEB_ACCESS(
-	ID SERIAL PRIMARY KEY,
-	IP4 nchar(15) NOT NULL,
-	IP6 nchar(39) NULL,
-	COUNTRY varchar(50) NULL,
-	COUNTRY_CODE nchar(3) NULL,
-	CITY varchar(50) NULL,
-	CONTINENT varchar(50) NULL,
-	LATITUDE numeric(9, 6) NULL,
-	LONGITUDE numeric(9, 6) NULL,
-	TIMEZONE varchar(50) NULL,
-	POSTAL_CODE varchar(10) NULL,
-	ISP_NAME varchar(50) NULL,
-	USER_AGENT varchar(512) NULL,
-	VISIT_DATETIME timestamp NOT NULL DEFAULT NOW()::timestamp,
-	VISIT_URL varchar(512) NULL
+CREATE TABLE public.web_access (
+    id integer NOT NULL,
+    ip4 character(15),
+    ip6 character(39),
+    country character varying(50),
+    country_code character(3),
+    city character varying(50),
+    continent character varying(50),
+    latitude numeric(9,6),
+    longitude numeric(9,6),
+    timezone character varying(50),
+    postal_code character varying(10),
+    isp_name character varying(50),
+    user_agent character varying(512),
+    visit_datetime timestamp without time zone DEFAULT (now())::timestamp without time zone NOT NULL,
+    visit_url character varying(512)
 );
+
+CREATE SEQUENCE public.web_access_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE ONLY public.web_access ALTER COLUMN id SET DEFAULT nextval('public.web_access_id_seq'::regclass);
+
+ALTER TABLE ONLY public.web_access
+    ADD CONSTRAINT web_access_pkey PRIMARY KEY (id);
